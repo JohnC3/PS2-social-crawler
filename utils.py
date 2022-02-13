7f69364e0d1b726930311e9d5dad0a2f91b7e6b5
John <jclements@ushrauto.com>
2022-02-13 17:05:30 -0500

    Updated utils

--- utils.py
+++ utils.py
@@ -4,19 +4,50 @@ import os
 import time
 import urllib.request
 
-logger = logging.getLogger(__name__)
-logging.basicConfig()
-logger.setLevel(logging.DEBUG)
-
 with open('local_config.json', 'r') as json_conf:
     config_dict = json.load(json_conf)
 
+LOGGER_NAME = 'ps2_crawler'
 SERVICE_ID = config_dict['service_id']
 DATABASE_PATH = os.path.join(*config_dict['database_path'])
 RETRY_CAP = config_dict['retry_cap']
-COOL_DOWN = 5.0
-MAX_INACTIVE_DAYS = 21
-FRIEND_BATCH_SIZE = 40
+COOL_DOWN = config_dict['cool_down']
+MAX_INACTIVE_DAYS = config_dict['max_inactive_days']
+FRIEND_BATCH_SIZE = config_dict['friend_batch_size']
+
+
+def setup_logger(level: str, log_file_name: str, server: str) -> None:
+    """
+    Setup a logger
+    """
+
+    if not log_file_name.endswith('.log'):
+        log_file_name = log_file_name + '.log'
+
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.setLevel(logging.DEBUG)
+    # Define the formatter
+    log_format = f"{server}: %(levelname)s - %(asctime)s - %(message)s"
+    formatter = logging.Formatter(log_format)
+    # Define the console logging
+    stream = logging.StreamHandler()
+    stream.setFormatter(formatter)
+    stream.setLevel(logging.DEBUG)
+    logger.addHandler(stream)
+
+    # Define the log file we want to output to
+    log_filer_handler = logging.FileHandler(log_file_name, mode='a')
+    log_filer_handler.setFormatter(formatter)
+    log_filer_handler.setLevel(logging.DEBUG)
+    logger.addHandler(log_filer_handler)
+
+
+def fetch_logger() -> logging.Logger:
+    """
+    Get the logger
+    """
+
+    return logging.getLogger(LOGGER_NAME)
 
 
 def single_column(conn, query):
@@ -33,7 +64,7 @@ class GiveUpException(Exception):
     """
 
 
-def fetch_url(url: str):
+def fetch_url(url: str) -> dict:
 
     # Fetch the data from url
     backoff = 4
@@ -44,30 +75,38 @@ def fetch_url(url: str):
             decoded = json.loads(jsonObj.read().decode("utf8"))
             if 'errorCode' in decoded:
                 if len(decoded.keys()) == 1:
-                    logger.error(f"Sever returned an error {decoded} when running {url}")
+                    fetch_logger().error(f"Sever returned an error {decoded} when running {url}")
                 else:
-                    logger.error(f"Sever partial error {decoded} when running {url}")
+                    fetch_logger().error(f"Sever partial error {decoded} when running {url}")
                 raise GiveUpException(f"GiveUpException: because {decoded}")
             return decoded
 
         except GiveUpException as stop:
             # Don't handle such exceptions
+            fetch_logger().error(f'Fatal exception raised {stop}')
             raise stop
 
-        except Exception as e:
-            if 'WinError 10054' in str(e):
-                # The connection has been terminated, don't know why, but stop harassing the server about it.
-                raise GiveUpException(str(e))
+        except (ConnectionResetError, Exception) as e:
 
-            notice_str = 'While requesting {} caught exception {}; retry after {}'.format(url, e, backoff)
-            if backoff < 10:
-                logger.debug(notice_str)
-            else:
-                logger.info(notice_str)
+            notice_str = f'While requesting {url} caught exception {e}; retry after {backoff}'
+            fetch_logger().error(notice_str)
             time.sleep(backoff)
             backoff *= 2
             retry_count += 1
     raise Exception(f"Unable to call fetch_url on url {url} see logs")
 
 
+def chunks(long_list, batch_size):
+    """
+    Breaks a list into a list of length batch_size lists.
+    """
+    list_of_batches = []
 
+    while len(long_list) > batch_size:
+
+        list_of_batches.append(long_list[:batch_size])
+        long_list = long_list[batch_size:]
+
+    if len(long_list) > 0:
+        list_of_batches.append(long_list)
+    return list_of_batches
